e692a5200a0ea4214e4d0ecccad628d29ff4778c
Ciaowilly3 <muscomassi@gmail.com>
2022-12-13 17:40:42 +0100

    poster comparsi e gestita l'assenza di immagine

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -3,6 +3,13 @@
     <ul>
       <li v-for="movie in store.filmsArray">
         {{ movie.title }}
+        <div class="poster-container">
+          <img
+            :src="imageCreator(movie.poster_path)"
+            alt=""
+            class="w-100 d-block"
+          />
+        </div>
         <ul>
           <li>Titolo originale: {{ movie.original_title }}</li>
           <li>
@@ -16,6 +23,13 @@
     <ul>
       <li v-for="serie in store.seriesArray">
         {{ serie.name }}
+        <div class="poster-container">
+          <img
+            :src="imageCreator(serie.poster_path)"
+            alt=""
+            class="w-100 d-block"
+          />
+        </div>
         <ul>
           <li>Titolo originale: {{ serie.original_name }}</li>
           <li>
@@ -28,7 +42,12 @@
     </ul>
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.poster-container {
+  width: 92px;
+  object-fit: contain;
+}
+</style>
 <script>
 import { store } from "../store";
 export default {
@@ -53,6 +72,12 @@ export default {
         lang = "in";
       }
       return "fi fi-" + lang;
+    },
+    imageCreator(url) {
+      if (!url) {
+        return "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restroom-out-of-order-door-sign-printable-design-template-cc665dd6f85fc0e9a7f116e283acc820_screen.jpg?ts=1637013157";
+      }
+      return "https://image.tmdb.org/t/p/" + "w92" + url;
     },
   },
 };
