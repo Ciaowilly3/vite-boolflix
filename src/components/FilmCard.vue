129baff7c2e4adb968559c7f457f0b99fa5da005
Ciaowilly3 <muscomassi@gmail.com>
2022-12-14 12:40:12 +0100

    tentativo di aggiunta componente per ogni card

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,45 +1,15 @@
 <template>
-  <div class="container d-flex">
-    <ul>
-      <li v-for="movie in store.filmsArray">
-        {{ movie.title }}
-        <div class="poster-container">
-          <img
-            :src="imageCreator(movie.poster_path)"
-            alt=""
-            class="w-100 d-block"
-          />
-        </div>
-        <ul>
-          <li>Titolo originale: {{ movie.original_title }}</li>
-          <li>
-            Lingua originale:
-            <span :class="flagCreator(movie.original_language)"></span>
-          </li>
-          <li>Media dei voti su TMDB: {{ movie.vote_average }}</li>
-        </ul>
-      </li>
-    </ul>
-    <ul>
-      <li v-for="serie in store.seriesArray">
-        {{ serie.name }}
-        <div class="poster-container">
-          <img
-            :src="imageCreator(serie.poster_path)"
-            alt=""
-            class="w-100 d-block"
-          />
-        </div>
-        <ul>
-          <li>Titolo originale: {{ serie.original_name }}</li>
-          <li>
-            Lingua originale:
-            <span :class="flagCreator(serie.original_language)"></span>
-          </li>
-          <li>Media dei voti su TMDB: {{ serie.vote_average }}</li>
-        </ul>
-      </li>
-    </ul>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3 col-2" v-for="movie in store.filmsArray">
+        <SingleCard :movie="movie"></SingleCard>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 col-2" v-for="serie in store.seriesArray">
+        <SingleCard :serie="serie"></SingleCard>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -50,35 +20,13 @@
 </style>
 <script>
 import { store } from "../store";
+import SingleCard from "./SingleCard.vue";
 export default {
+  components: { SingleCard },
   data() {
     return {
       store,
     };
-  },
-  methods: {
-    flagCreator(lang) {
-      // inserisco una sfilza di if per gestire i linguaggi non inserito con il codice alpha 2 usato dalla mia npm
-      if (lang === "en") {
-        lang = "gb";
-      }
-      if (lang === "ja") {
-        lang = "jp";
-      }
-      if (lang === "ko") {
-        lang = "kr";
-      }
-      if (lang === "hi") {
-        lang = "in";
-      }
-      return "fi fi-" + lang;
-    },
-    imageCreator(url) {
-      if (!url) {
-        return "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restroom-out-of-order-door-sign-printable-design-template-cc665dd6f85fc0e9a7f116e283acc820_screen.jpg?ts=1637013157";
-      }
-      return "https://image.tmdb.org/t/p/" + "w92" + url;
-    },
   },
 };
 </script>
