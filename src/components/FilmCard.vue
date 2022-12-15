0f0e8900b8f7ff6de4aa3a26df2894036a50e6c1
Ciaowilly3 <muscomassi@gmail.com>
2022-12-15 17:45:25 +0100

    comparsa stelle

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -11,7 +11,18 @@
           <span :class="flagCreator(movie.original_language)"></span>
         </li>
 
-        <li>Media dei voti su TMDB: {{ movie.vote_average }}</li>
+        <li>
+          Media dei voti su TMDB: {{ movie.vote_average }}
+          <i
+            class="fa-star"
+            v-for="star in starBuilder"
+            :class="{
+              fa: star === true,
+              'fa-regular': star === false,
+              'fa-solid fa-star fa-star-half-stroke': star === 'half',
+            }"
+          ></i>
+        </li>
       </ul>
       <h6 class="text-center mt-3 mb-2 fs-5">Overview</h6>
       <div class="p-container">
@@ -92,6 +103,22 @@ export default {
         return "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restroom-out-of-order-door-sign-printable-design-template-cc665dd6f85fc0e9a7f116e283acc820_screen.jpg?ts=1637013157";
       }
       return "https://image.tmdb.org/t/p/" + "w342" + url;
+    },
+  },
+  computed: {
+    starBuilder() {
+      const toReturn = [];
+      const rawVote = this.movie.vote_average;
+      const halfVote = Math.floor(rawVote / 2);
+      const decimal = (rawVote / 2) % 1;
+      for (let i = 0; i < 5; i++) {
+        let toPush = i < halfVote;
+        if (halfVote === i && decimal) {
+          toPush = "half";
+        }
+        toReturn.push(toPush);
+      }
+      return toReturn;
     },
   },
 };
--- src/components/SerieCard.vue
+++ src/components/SerieCard.vue
@@ -1,18 +1,74 @@
 <template>
-  <img :src="imageCreator(serie.poster_path)" alt="" class="w-100 d-block" />
-  <div class="info-container">
-    {{ serie.name }}
-    Titolo originale: {{ serie.original_name }}
+  <div class="card-container">
+    <img :src="imageCreator(serie.poster_path)" alt="" class="w-100 d-block" />
 
-    Lingua originale:
-    <span :class="flagCreator(serie.original_language)"></span>
+    <div class="info-container">
+      <h5 class="text-center my-3 fs-4">{{ serie.name }}</h5>
+      <ul>
+        <li>Titolo originale: {{ serie.original_name }}</li>
 
-    Media dei voti su TMDB: {{ serie.vote_average }}
+        <li>
+          Lingua originale:
+          <span :class="flagCreator(serie.original_language)"></span>
+        </li>
+
+        <li>
+          Media dei voti su TMDB: {{ serie.vote_average
+          }}<i
+            class="fa-star"
+            v-for="star in starBuilder"
+            :class="{
+              fa: star === true,
+              'fa-regular': star === false,
+              'fa-solid fa-star fa-star-half-stroke': star === 'half',
+            }"
+          ></i>
+        </li>
+      </ul>
+      <h6 class="text-center mt-3 mb-2 fs-5">Overview</h6>
+      <div class="p-container">
+        <p class="text-center mx-1" v-if="serie.overview">
+          {{ serie.overview }}
+        </p>
+        <p v-else class="text-danger text-center fs-3">Overview unavailable!</p>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.info-container {
-  display: none;
+.card-container {
+  width: 100%;
+  position: relative;
+  .p-container {
+    overflow: auto;
+    height: 50%;
+  }
+  img {
+    object-position: top;
+    display: block;
+  }
+  .info-container {
+    width: 100%;
+    height: 100%;
+    background-color: aliceblue;
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 5;
+    li {
+      margin: 4px 0;
+      color: rgb(87, 80, 80);
+    }
+  }
+}
+.card-container:hover {
+  img {
+    display: none;
+  }
+  .info-container {
+    display: block;
+  }
 }
 </style>
 <script>
@@ -48,6 +104,22 @@ export default {
         return "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/restroom-out-of-order-door-sign-printable-design-template-cc665dd6f85fc0e9a7f116e283acc820_screen.jpg?ts=1637013157";
       }
       return "https://image.tmdb.org/t/p/" + "w342" + url;
+    },
+  },
+  computed: {
+    starBuilder() {
+      const toReturn = [];
+      const rawVote = this.serie.vote_average;
+      const halfVote = Math.floor(rawVote / 2);
+      const decimal = (rawVote / 2) % 1;
+      for (let i = 0; i < 5; i++) {
+        let toPush = i < halfVote;
+        if (halfVote === i && decimal) {
+          toPush = "half";
+        }
+        toReturn.push(toPush);
+      }
+      return toReturn;
     },
   },
 };
